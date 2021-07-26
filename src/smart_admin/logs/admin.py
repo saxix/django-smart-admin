09f2ef0d536113a9cbb3385a81c4ef0a2557feb2
sax <s.apostolico@gmail.com>
2021-07-26 16:01:01 +0200

    updates

--- src/smart_admin/logs/admin.py
+++ src/smart_admin/logs/admin.py
@@ -10,8 +10,15 @@ from smart_admin.mixins import SmartMixin
 
 @register(LogEntry)
 class LogEntryAdmin(SmartMixin, admin.ModelAdmin):
+    list_display = ('action_time', 'user',  'action_flag', 'content_type', 'object_repr',)
     readonly_fields = ('__all__',)
+    search_fields = ('object_repr', )
     list_filter = (('user', AutoCompleteFilter),
                    ('content_type', RelatedFieldComboFilter),
                    'action_flag')
     date_hierarchy = 'action_time'
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        qs.select_related('content_type', 'user')
+        return qs
