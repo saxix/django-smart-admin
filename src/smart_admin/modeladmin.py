c47b0f0a221527961acd9e1e02305fda5d696828
Stefano Apostolico <s.apostolico@gmail.com>
2025-04-28 17:54:57 +0200

    restored modeladmin

---
+++ src/smart_admin/modeladmin.py
@@ -0,0 +1,9 @@
+from django.contrib.admin import ModelAdmin
+
+from smart_admin.changelist import SmartChangeListMixin
+from smart_admin.mixins import SmartMixin
+
+
+class SmartModelAdmin(SmartMixin, SmartChangeListMixin, ModelAdmin):
+    def get_list_filter(self, request):
+        return super().get_list_filter(request)
